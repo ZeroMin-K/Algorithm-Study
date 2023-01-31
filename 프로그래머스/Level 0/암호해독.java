f7d1573982361b56d8c8285e03c351057255765c
ZeroMin-K <ymkim9510@gmail.com>
2023-01-31 20:10:36 +0900

    Algorithm Study - 프로그래머스 문제 풀이

---
+++ 프로그래머스/Level 0/암호해독.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        int len = cipher.length();
+        
+        for (int i = code - 1; i < len; i += code) {
+            answer = answer + cipher.charAt(i);
+        }
+        
+        return answer;
+    }
+}
