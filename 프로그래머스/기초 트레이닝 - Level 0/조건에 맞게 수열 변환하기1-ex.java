591ba1683754f4fe6f6ad65b89785d4f19a5700d
ZeroMin-K <ymkim9510@gmail.com>
2023-06-07 22:53:58 +0900

    Algorithm Study - 프로그래머스 - 기초 트레이닝- 조건에 맞게 수열 변환1

---
+++ 프로그래머스/기초 트레이닝 - Level 0/조건에 맞게 수열 변환하기1-ex.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        return Arrays.stream(arr)
+                    .map(operand -> 
+                         operand >= 50 && operand % 2 == 0 ? 
+                         operand / 2 : operand < 50 && operand % 2 == 1 ? operand * 2 : operand)
+                    .toArray();
+    }
+}
