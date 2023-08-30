60ea42d77a7dd5c339f2d3827469ab4bc8b0b43b
ZeroMin-K <ymkim9510@gmail.com>
2023-08-30 20:33:26 +0900

    Algorithm Study - 프로그래머스 - 기초 트레이닝 - 세로 읽기

---
+++ 프로그래머스/기초 트레이닝 - Level 0/세로 읽기-ex.java
@@ -0,0 +1,10 @@
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        return IntStream.range(0, my_string.length()) 
+                .filter(i -> i % m == c - 1)
+                .mapToObj(i -> String.valueOf(my_string.charAt(i)))
+                .collect(Collectors.joining());
+    }
+}
