3ca675ede44bc10d379cf6e5bfd69197043f4f8e
ZeroMin-K <ymkim9510@gmail.com>
2023-10-17 20:19:06 +0900

    Algorithm Study - 프로그래머스 - 기초 트레이닝 - 정사각형으로 만들기

---
+++ 프로그래머스/기초 트레이닝 - Level 0/정사각형으로 만들기-solve.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] solution(int[][] arr) {
+        if (arr.length == arr[0].length) return arr; 
+        
+        int length = Math.max(arr.length, arr[0].length);
+        int[][] answer = new int[length][length];
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                answer[i][j] = arr[i][j]; 
+            }
+        }
+        
+        return answer;
+    }
+}
---
+++ 프로그래머스/기초 트레이닝 - Level 0/정사각형으로 만들기-solve.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    if len(arr) == len(arr[0]):
+        return arr
+    
+    n = max(len(arr), len(arr[0]))
+    answer = [[0] * n for _ in range(n)]
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            answer[i][j] = arr[i][j] 
+        
+    return answer
